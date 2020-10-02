4c958dfc4f09fed5bc05c852fe10007e4c921ca8
balez4464 <balez4464@wrdsb.ca>
2020-10-02 13:40:16 +0000

    made an example for while loops

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 /**
- *
- * @author 
+ *This is an example for while loops
+ * @author Zachary Balean
  */
 public class Main {
 
@@ -9,7 +9,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    System.out.println("Beginning to count!");
+    //create a counter variable
+    int count = 1;
+    //use a loop to count to five
+    while(count <= 5){
+      //print number we are on
+      System.out.println(count);
+      //adds number
+      count = count + 1;
+    }
+    System.out.println("That's all for now");
+
+    int count2 = 1;
+
+    do{
+      System.out.println(count2);
+      count2 = count2 + 1;
+    }while(count2 <= 5);
+
+
+
+
   }
 }
